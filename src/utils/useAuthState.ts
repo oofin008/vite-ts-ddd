b60171c3a6485ae5c851039d9fa87364de43e24f
Santi Liwsakphaiboon <oofpokemon@gmail.com>
2022-07-07 12:35:17 +0700

    feat: utils for auth hooks

---
+++ src/utils/useAuthState.ts
@@ -0,0 +1,43 @@
+import { Auth, onAuthStateChanged, User } from 'firebase/auth';
+import { useEffect, useMemo } from 'react';
+import { LoadingHook, useLoadingValue } from './useLoadingValue';
+
+export type AuthStateHook = LoadingHook<User | null, Error>;
+
+// Ref: https://github.com/CSFrequency/react-firebase-hooks/tree/edab3f3f3b5ec01c8aafcc6096755dfcc69e4408
+
+type AuthStateOptions = {
+  onUserChanged?: (user: User | null) => Promise<void>;
+};
+
+export default (auth: Auth, options?: AuthStateOptions): AuthStateHook => {
+  const { error, loading, setError, setValue, value } = useLoadingValue<
+    User | null,
+    Error
+  >(() => auth.currentUser);
+
+  useEffect(() => {
+    const listener = onAuthStateChanged(
+      auth,
+      async (user) => {
+        if (options?.onUserChanged) {
+          // onUserChanged function to process custom claims on any other trigger function
+          try {
+            await options.onUserChanged(user);
+          } catch (e) {
+            setError(e as Error);
+          }
+        }
+        setValue(user);
+      },
+      setError
+    );
+
+    return () => {
+      listener();
+    };
+  }, [auth]);
+
+  const resArray: AuthStateHook = [value, loading, error];
+  return useMemo<AuthStateHook>(() => resArray, resArray);
+};
