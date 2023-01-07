bb01165a771d9b23c173412241027973f7e750ac
Santi Liwsakphaiboon <oofpokemon@gmail.com>
2023-01-07 22:00:06 +0700

    fix: invalid state chart

--- src/core/presentation/auth/authMachine.ts
+++ src/core/presentation/auth/authMachine.ts
@@ -16,6 +16,7 @@ export const authenticationMachine =
       states: {
         checkingIfLoggedIn: {
           invoke: {
+            id: "checkIfLoggedIn",
             src: "checkIfLoggedIn",
             onDone: {
               target: "loggedIn",
