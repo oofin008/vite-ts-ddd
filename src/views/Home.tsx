4f1b5f47498f272c2806962b51710407416b84f0
Santi Liwsakphaiboon <oofpokemon@gmail.com>
2023-01-09 22:41:36 +0700

    chores: remove exposed token

--- src/views/Home.tsx
+++ src/views/Home.tsx
@@ -6,7 +6,6 @@ const Home = () => {
   return (
     <>
       <div className='test-scss'>Home</div>
-      <div>secret_56y4TcFwmDscWPrVrpImVuuxv6wCCvFw10JKLOIVVAP</div>
 
       <Suspense fallback={<h1>Loading..</h1>}>
         <Blog/>
