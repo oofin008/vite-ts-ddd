b8d0eb158e8939764d83ded769ad8efb0474781e
Santi Liwsakphaiboon <oofpokemon@gmail.com>
2023-08-05 22:21:06 +0700

    feat: add eslint

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,6 @@
 import react from "@vitejs/plugin-react";
 import { defineConfig, loadEnv } from "vite";
+import eslint from "vite-plugin-eslint"
 import vitePluginImp from "vite-plugin-imp";
 
 const path = require("path");
@@ -7,10 +8,11 @@ const path = require("path");
 // https://vitejs.dev/config/
 export default ({ mode }) => {
   process.env = { ...process.env, ...loadEnv(mode, process.cwd()) };
-  // console.log(process.env);
+
   return defineConfig({
     plugins: [
       react(),
+      eslint(),
       vitePluginImp({
         libList: [
           { libName: 'antd', style: (name) => `antd/es/${name}/style` },
