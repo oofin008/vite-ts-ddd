f111f7258e69f25a7bd415566c0b38d39cd8dd0e
Santi Liwsakphaiboon <oofpokemon@gmail.com>
2024-09-16 22:55:12 +0700

    feat: permission guard with role base

--- src/components/Guard/PermissionGuard.tsx
+++ src/components/Guard/PermissionGuard.tsx
@@ -1,36 +1,48 @@
-import React, { Suspense, useContext } from 'react'
+import React, { Suspense, useContext, useEffect } from 'react'
 import { Alert, Spin } from 'antd'
 import { Navigate } from 'react-router-dom';
 import { AuthMachineContext } from '@/core/presentation/auth/authMachine'
-import { AuthContext } from '@/core/types/authentication';
+import { ALL_ROLES, AuthContext } from '@/core/types/authentication';
 import { Permission, PermissionGuardProps } from './type';
+
+const roleComparer = (role: string | undefined, permission: Permission) => {
+  let isAbleToAccess = false;
+  if (role === undefined) {
+    return false;
+  }
+  if (role === "admin" || permission === Permission["PUBLIC"]) {
+    isAbleToAccess = true;
+  }
+  if (role === "user" && permission !== Permission["ADMIN"]) {
+    isAbleToAccess = true;
+  }
+  if (!ALL_ROLES.includes(role)) {
+    isAbleToAccess = false;
+  }
+  return isAbleToAccess;
+}
 
 export const PermissionGuard: React.FC<PermissionGuardProps> = (props) => {
   const [state, send, service] = useContext<AuthContext>(AuthMachineContext);
-  const { permission } = props;
-  // const { isLoginError } = state.context;
 
-  switch (permission) {
-    case Permission["PUBLIC"]:
-      return (<Suspense fallback={<Spin tip='loading...' />}>
-        {props.children}
-      </Suspense>);
-    case Permission["USER"]:
-    case Permission["ADMIN"]:
-    default:
-      return (
-        <>
-          {state.hasTag('loading') && <Spin tip='Loading...' />}
-          {state.matches('loggedIn') &&
-            <Suspense fallback={<Spin tip='loading...' />}>
-              {props.children}
-            </Suspense>
-          }
-          {state.matches('loggedOut') &&
-            <div>You are not login, please login to access this content.</div>
-          }
-          {/* {isLoginError && <Alert type="error" message="Login Error" banner closable/>} */}
-        </>
-      )
-  }
+  const { permission } = props;
+
+  return (
+    <>
+      {state.hasTag('loading') && <Spin tip='Loading...' />}
+      {state.matches('loggedIn') && (roleComparer(state.context.role, permission) ?
+        <Suspense fallback={<Spin tip='loading...' />}>
+          {props.children}
+        </Suspense>
+        : <div>You are not authorized to access this content.</div>
+      )}
+      {state.matches('loggedOut') && (permission !== Permission["PUBLIC"] ?
+        <div>You are not login, please login to access this content.</div>
+        : <Suspense fallback={<Spin tip='loading...' />}>
+          {props.children}
+        </Suspense>
+      )}
+      {/* {isLoginError && <Alert type="error" message="Login Error" banner closable/>} */}
+    </>
+  )
 }
